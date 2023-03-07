f2e98930d84e79096a5b113333c675a66f74fb1c
Mess <alla-fedia@mail.ru>
2023-03-07 15:20:06 +0300

    квардрат числа

--- sem_1/Program.cs
+++ sem_1/Program.cs
@@ -3,3 +3,9 @@ int num = Convert.ToInt32( Console.ReadLine() );
 int res = num * num;
 Console.WriteLine("Квадрат введенного числа равен "+res);
 
+if (num==5)
+{
+    Console.WriteLine("Пятерочка!");
+}
+else
+    Console.WriteLine("Не пятерочка!");
