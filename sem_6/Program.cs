65022e353d3d9200d94973a6a9e4cb2305304bdd
Mess <alla-fedia@mail.ru>
2023-03-07 18:13:54 +0300

    последняя цифра в числе

---
+++ sem_6/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("введите 3х значное число");
+int num = Convert.ToInt32(Console.ReadLine());
+int ost = num % 10;
+Console.WriteLine(ost);
+
+// показывает последнюю цифру в числе
