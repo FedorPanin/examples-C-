354a8b76640cc54fcff1fbb7ab008d2462f22465
Mess <alla-fedia@mail.ru>
2023-03-07 19:03:00 +0300

    вывод 3тей цифры слева

---
+++ sem_7/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("введите любое число больше 3х знаков");
+int num = Convert.ToInt32(Console.ReadLine());
+int ostatok = 0;
+if (num < 100)
+{
+    Console.WriteLine("в вашем числе меньше 3х знаков");
+}
+else
+    if (num <= 999)
+    {
+    ostatok = num % 10;  
+    }
+    else
+        while (num > 999)
+        {
+            num = (num / 10);
+            ostatok = num % 10;
+        }
+Console.WriteLine(ostatok);
+
+// прорамма выводи 3-ю цифру слева в любом числе
