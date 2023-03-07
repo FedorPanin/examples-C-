fd7bfd95f83ab4eaab634d0d6e7085224a9362a7
Mess <alla-fedia@mail.ru>
2023-03-07 15:36:37 +0300

    выявление является ли одно исло корнем второго

---
+++ sem_2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("большее число");
+int num1 = Convert.ToInt32( Console.ReadLine() );
+Console.WriteLine("меньшее число");
+int num2 = Convert.ToInt32( Console.ReadLine() );
+
+if (num1 == num2 * num2)
+{
+    Console.WriteLine("является корнем");
+}
+else
+    Console.WriteLine("НЕ является!");
