4e4aa02ec627bb94e1286d26420156cbf88a8bf2
Mess <alla-fedia@mail.ru>
2023-03-08 15:41:43 +0300

    поиск нужного чила в массиве

---
+++ Les_2_ex_2/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 1, 12, 31, 4, 5, 6, 7, 12 };
+
+int n = array.Length;
+int find = 12;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;   // прерывает поиск в случае если несколько элементов имеют одинаковое значение, останавливается на первом
+    }
+    index++;
+}
