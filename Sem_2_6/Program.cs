93b6d9420fd1a60f53c9382a45be3b3f44b812d8
Mess <alla-fedia@mail.ru>
2023-03-14 15:15:34 +0300

    способ убрать второй символ из числа через модуль и деление остатка.

---
+++ Sem_2_6/Program.cs
@@ -0,0 +1,17 @@
+﻿int num = new Random().Next(1, 100000);
+Console.WriteLine($"вводное {num}");
+int firstnum;
+int secondnum;
+string pasnum = "";
+if (num > 10)
+{
+    while (num > 99)
+    {
+        pasnum = Convert.ToString(num % 10) + pasnum;
+        num = num / 10;
+    }
+    secondnum = num % 10;
+    firstnum = num /10;
+    Console.WriteLine($"результат {firstnum}{pasnum}");
+}
+ // способ убрать второй символ из числа через модуль и деление остатка.
