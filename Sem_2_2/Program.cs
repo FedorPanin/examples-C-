8a1516c8a92eef680d6d2ecb73c50226544d090b
Mess <alla-fedia@mail.ru>
2023-03-14 10:03:09 +0300

    выводим пределеные символы числа(за исключением второго)

---
+++ Sem_2_2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32 (Console.ReadLine());
+
+if (num > 999)
+    Console.WriteLine("error");
+else if (num < 100)
+    Console.WriteLine("error");
+
+string i = Convert.ToString(num);
+
+Console.Write(i[0].ToString() + i[2].ToString());
+
+
+// При вводе любого числа(в данном случае 3х значного)
+// Выводим в консоль цифры все кроме той которую не хотим показввать
+// В данном случает убирается вторая цифра числа
+// через параметр i[].Tostring() делается вывод определенного символа под индексом[]
