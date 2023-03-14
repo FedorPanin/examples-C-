c171baf1931f36129ee47cd8bbac606c5f359909
Mess <alla-fedia@mail.ru>
2023-03-14 10:31:55 +0300

    проверка на кратность одного числа другим, с выводом остатка.

---
+++ Sem_2_3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int res = num1 % num2;
+if (res == 0)
+    Console.WriteLine($"{num1} кратно {num2}");
+else 
+    Console.WriteLine($"{num1} не кратно {num2}, остаток " + res);
+
+
+// проверка на кратность одного числа другим, с выводом остатка.
