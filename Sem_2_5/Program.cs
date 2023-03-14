4e77a5e4906b5549d98f72e265a117e6bd16dd06
Mess <alla-fedia@mail.ru>
2023-03-14 14:38:38 +0300

    проверка числа на кратность сразу 2м числам

---
+++ Sem_2_5/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int x = num % 7;
+int z = num % 23;
+if ( x + z == 0 )
+    Console.WriteLine("кратно 7 и 23");
+else
+    Console.WriteLine("не кратно");
+
+// проверка числа на кратность сразу 2м числам 
